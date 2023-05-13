435745aff68aaf63a361a79f4cd99e4b259877f5
Trevor Jameson <tjameson15@gmail.com>
2023-05-13 10:06:14 -0400

    Completed review of project submission. See comments marked REVIEW:

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,7 +4,8 @@
 // How does each interaction update the state?
 // In many ways, this is the "game loop": show state -> wait for input -> update state -> go to step 1
 
-
+// REVIEW: Get into the habit of setting selected html elements to local vars instead of globals.
+// ...The globals won't update if you add / remove elements to the page.
 const cells = document.querySelectorAll('.box');
 const announcementBanner = document.querySelector('#announceBanner');
 const restartButton = document.querySelector('#restartButton');
@@ -36,10 +37,13 @@ buildInitialState();
 function buildInitialState() {
       cells.forEach(cell => cell.addEventListener('click',clickedCell));
       restartButton.addEventListener('click', restartGame);
+      // REVIEW: If this is the initial state of the page, does the banner text need to be set here
+      // ...instead of the html. Could the gameActive state also be initialized with let gameActive = true?
       announcementBanner.textContent = `${currentPlayer}'s Turn`;
       gameActive = true;
 }
 
+// REVIEW: What is the value of this? Could you use an event instead?
 function clickedCell(){
       const cellIndex = this.getAttribute("cellIndex");
       if(board[cellIndex] != "" || !gameActive){
@@ -69,9 +73,11 @@ function checkWinner(){
         const cell2 = board[condition[1]];
         const cell3 = board[condition[2]];
 
+        // REVIEW: Good accounting for edge case comparison
         if(cell1 == "" || cell2 == "" || cell3 == ""){
           continue;
         }
+        // REVIEW: Always use strict equals === instead of loose equals ==
         if (cell1 == cell2 && cell2 == cell3){
           matchWon = true;
           break;
